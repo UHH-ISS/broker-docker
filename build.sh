e2ae36ed31ae9c26f1f4ae21fea4c08bda697f87
Florian Wilkens <gh@1wilkens.org>
2020-04-09 12:03:11 +0200

    Add optional push to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+__push__="$1"
 
 # Pull alpine to get newest security updates
 docker pull alpine:3.10
@@ -6,7 +8,13 @@ docker pull alpine:3.10
 # Build all tags
 for TAG in $(cat tags.txt); do
     docker build --build-arg "BROKER_VERSION=v$TAG" -t "uhhiss/broker-docker:$TAG" .
+    if [ "$__push__" == "push" ]; then
+        docker push "uhhiss/broker-docker:$TAG"
+    fi
 done
 
 # Also tag latest
 docker build --build-arg "BROKER_VERSION=v$TAG" -t "uhhiss/broker-docker:latest" .
+if [ "$__push__" == "push" ]; then
+    docker push uhhiss/broker-docker:latest
+fi
