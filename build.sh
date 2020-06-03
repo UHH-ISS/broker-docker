c631cf2581a653f404080783a1c38c3f8ddb17f9
Florian Wilkens <wilkens@informatik.uni-hamburg.de>
2020-06-03 14:25:03 +0200

    Support broker 1.2 and 1.3 branches

--- build.sh
+++ build.sh
@@ -4,17 +4,26 @@ __push__="$1"
 
 # Pull alpine to get newest security updates
 docker pull alpine:3.10
+docker pull alpine:3.11
 
-# Build all tags
-for TAG in $(cat tags.txt); do
-    docker build --build-arg "BROKER_VERSION=v$TAG" -t "uhhiss/broker-docker:$TAG" .
+# Build tags for version 1.2.x
+for TAG in $(cat tags-1.2.txt); do
+    docker build --build-arg "BROKER_VERSION=v$TAG" -t "uhhiss/broker-docker:$TAG" -f Dockerfile-1.2 .
+    if [ "$__push__" == "push" ]; then
+        docker push "uhhiss/broker-docker:$TAG"
+    fi
+done
+
+# Build tags for version 1.2.x
+for TAG in $(cat tags-1.3.txt); do
+    docker build --build-arg "BROKER_VERSION=v$TAG" -t "uhhiss/broker-docker:$TAG" -f Dockerfile-1.3 .
     if [ "$__push__" == "push" ]; then
         docker push "uhhiss/broker-docker:$TAG"
     fi
 done
 
 # Also tag latest
-docker build --build-arg "BROKER_VERSION=v$TAG" -t "uhhiss/broker-docker:latest" .
+docker tag "uhhiss/broker-docker:$TAG" "uhhiss/broker-docker:latest"
 if [ "$__push__" == "push" ]; then
     docker push uhhiss/broker-docker:latest
 fi
